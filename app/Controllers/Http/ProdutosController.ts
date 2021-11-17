56ba373e84344dd49d86a717c5134deefc2f2c6a
EriksJunior <EriksJunior13@gmail.com>
2021-11-16 23:12:33 -0300

    commit

--- app/Controllers/Http/ProdutosController.ts
+++ app/Controllers/Http/ProdutosController.ts
@@ -2,25 +2,30 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Produto from '../../Models/Produto'
 
 export default class ProdutosController {
-  public async index ({}: HttpContextContract) {
-    
+  public async index({ }: HttpContextContract) {
+    try {
+      const dadosGerais = await Produto.all()
+      return dadosGerais
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({ }: HttpContextContract) {
   }
 
-  public async store ({}: HttpContextContract) {
+  public async store({ }: HttpContextContract) {
   }
 
-  public async show ({}: HttpContextContract) {
+  public async show({ }: HttpContextContract) {
   }
 
-  public async edit ({}: HttpContextContract) {
+  public async edit({ }: HttpContextContract) {
   }
 
-  public async update ({}: HttpContextContract) {
+  public async update({ }: HttpContextContract) {
   }
 
-  public async destroy ({}: HttpContextContract) {
+  public async destroy({ }: HttpContextContract) {
   }
 }
