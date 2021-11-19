f7b90476dc3cf7017b00dd3fa8483cacab06639a
EriksJunior <EriksJunior13@gmail.com>
2021-11-18 21:18:11 -0300

    commit

--- app/Controllers/Http/ProdutosController.ts
+++ app/Controllers/Http/ProdutosController.ts
@@ -15,7 +15,7 @@ export default class ProdutosController {
     try {
       const dadosProdutos = request.only(['nome', 'marca', 'quantidade', 'preco_custo', 'preco_venda', 'categoria', 'data_cadastro', 'obs'])
       const data = await Produto.create(dadosProdutos)
-      response.status(200).json({ message: 'Cliente salvo com sucesso!' })
+      response.status(200).json({ message: 'Produto salvo com sucesso!' })
       console.log(data)
       return data
     } catch (error) {
@@ -28,9 +28,28 @@ export default class ProdutosController {
     return dadosProdutos
   }
 
-  public async update({ }: HttpContextContract) {
+  public async update({ request, response, params }: HttpContextContract) {
+    try {
+      const data = await Produto.findOrFail(params.id)
+      const dadosProduto = request.only(['nome', 'marca', 'quantidade', 'preco_custo', 'preco_venda', 'categoria', 'data_cadastro', 'obs'])
+      data.merge(dadosProduto)
+
+      await data.save()
+      response.status(200).json({ message: 'Produto atualizado com sucesso' })
+      return data
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  public async destroy({ }: HttpContextContract) {
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const data = await Produto.findOrFail(params.id)
+      data.delete()
+      response.status(200).json({ message: 'Produto deletado com sucesso!' })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
