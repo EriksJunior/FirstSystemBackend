7a7a66a9c01845c5073f0d616bc27f3452037cca
EriksJunior <EriksJunior13@gmail.com>
2021-11-26 19:51:37 -0300

    commit

--- app/Controllers/Http/FornecedoresController.ts
+++ app/Controllers/Http/FornecedoresController.ts
@@ -12,6 +12,16 @@ export default class FornecedoresController {
     }
   }
 
+  public async buscarFornecedor({ params }: HttpContextContract) {
+    try {
+      const dadosFornecedor = await Fornecedor.findOrFail(params.id)
+      console.log(dadosFornecedor)
+      return dadosFornecedor
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   public async store({ request }: HttpContextContract) {
     const pegarDadosFornecedor = await request.only(['razao_social', 'nome_fantasia', 'endereco', 'bairro', 'numero', 'cidade', 'uf', 'cnpj', 'ie', 'telefone'])
     const saveFornecedor = await Fornecedor.create(pegarDadosFornecedor)
--- start/routes/fornecedor.ts
+++ start/routes/fornecedor.ts
@@ -2,6 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 
 Route.get('/fornecedor', 'FornecedoresController.index')
+Route.get('/fornecedor/:id', 'FornecedoresController.buscarFornecedor')
 Route.post('/fornecedor', 'FornecedoresController.store')
-
 export default Route
