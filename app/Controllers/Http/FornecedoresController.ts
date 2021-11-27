9ccb0d5d282e615cb4f6a878f6274d1f13cc3442
EriksJunior <EriksJunior13@gmail.com>
2021-11-27 13:16:56 -0300

    commit

--- app/Controllers/Http/FornecedoresController.ts
+++ app/Controllers/Http/FornecedoresController.ts
@@ -40,6 +40,7 @@ export default class FornecedoresController {
 
     idFornecedor.merge(dadosFornecedor)
     await idFornecedor.save();
+    return idFornecedor
 
   }
 
--- start/routes/fornecedor.ts
+++ start/routes/fornecedor.ts
@@ -4,4 +4,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/fornecedor', 'FornecedoresController.index')
 Route.get('/fornecedor/:id', 'FornecedoresController.buscarFornecedor')
 Route.post('/fornecedor', 'FornecedoresController.store')
+Route.put('/fornecedor/:id', 'FornecedoresController.update')
 export default Route
