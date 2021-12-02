b23ff3018e051cbff9d63229bdf16f853f8b74db
EriksJunior <EriksJunior13@gmail.com>
2021-12-01 23:33:09 -0300

    commit

--- start/routes/mov_estoque.ts
+++ start/routes/mov_estoque.ts
@@ -1,7 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/movestoque', 'MovEstoquesController.index');
+Route.get('/movestoque/:id', 'MovEstoquesController.update');
 Route.post('/movestoque', 'MovEstoquesController.store');
+Route.put('/movestoque/:id', 'MovEstoquesController.update');
+Route.delete('/movestoque/:id', 'MovEstoquesController.destroy');
 
 
 export default Route
