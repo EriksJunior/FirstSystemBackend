b847a3e92445bb6d3ff22e25552dc6c25d0f2b11
EriksJunior <EriksJunior13@gmail.com>
2021-12-04 20:24:19 -0300

    commit

--- app/Controllers/Http/MovEstoquesController.ts
+++ app/Controllers/Http/MovEstoquesController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import MovEstoque from '../../Models/MovEstoque'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class MovEstoquesController {
   public async index({ }: HttpContextContract) {
@@ -24,8 +25,8 @@ export default class MovEstoquesController {
     try {
       const dadosMovimentacaoEstoque = await request.only(['id_produto', 'id_fornecedor', 'quantidade', 'numero_nfe', 'tipo_movimentacao'])
       const data = await MovEstoque.create(dadosMovimentacaoEstoque);
-      const teste = await MovEstoque.query().select(['numero_nfe']).where(['numero_nfe', '=', dadosMovimentacaoEstoque.numero_nfe])
-      console.log(teste)
+      const quantProdutos = await Database.rawQuery(`select sum(quantidade) from mov_estoques where id_produto = ${dadosMovimentacaoEstoque.id_produto}`)
+      console.log(quantProdutos)
       return data
     } catch (error) {
       console.log(error)
