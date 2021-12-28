41f205319c9b50ca2d9d394b2aaaa16f5c457927
EriksJunior <EriksJunior13@gmail.com>
2021-12-27 21:32:50 -0300

    commit

--- app/Models/MovVenda.ts
+++ app/Models/MovVenda.ts
@@ -1,9 +1,27 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, BelongsTo, belongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Produto from '../Models/Produto'
+import Cliente from '../Models/Cliente'
+import Venda from '../Models/Venda'
 
 export default class MovVenda extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id_mov_venda: number
+
+  @column()
+  public id_produto: number
+  @belongsTo(() => Produto, { foreignKey: 'id_produto' })
+  public produto: BelongsTo<typeof Produto>
+
+  @column()
+  public id_cliente: number
+  @belongsTo(() => Cliente, { foreignKey: 'id_cliente' })
+  public cliente: BelongsTo<typeof Cliente>
+
+  @column()
+  public id_venda: number
+  @belongsTo(() => Venda, { foreignKey: 'id_venda' })
+  public venda: BelongsTo<typeof Venda>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
--- app/Models/Venda.ts
+++ app/Models/Venda.ts
@@ -3,7 +3,16 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Venda extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id_venda: number
+
+  @column()
+  public tipoVenda: string
+
+  @column()
+  public data_venda: DateTime
+
+  @column()
+  public data_entrega: DateTime
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
