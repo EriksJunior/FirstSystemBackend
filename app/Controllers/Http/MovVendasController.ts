0f8eb6777f009745ddd5dd6c49e55817e905af2c
EriksJunior <EriksJunior13@gmail.com>
2021-12-31 00:24:01 -0300

    commit

--- app/Controllers/Http/MovVendasController.ts
+++ app/Controllers/Http/MovVendasController.ts
@@ -1,24 +1,30 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MovEstoque from '../../Models/MovVenda'
 
 export default class MovVendasController {
-  public async index ({}: HttpContextContract) {
+  public async index({ }: HttpContextContract) {
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({ }: HttpContextContract) {
   }
 
-  public async store ({}: HttpContextContract) {
+  public async store({ request }: HttpContextContract) {
+
+    const dadosMovVendas = await request.only(['id_venda', 'id_produto', 'quantidade', 'valor', 'unidade'])
+    const data = MovEstoque.create(dadosMovVendas)
+    console.log(data)
+    return data
   }
 
-  public async show ({}: HttpContextContract) {
+  public async show({ }: HttpContextContract) {
   }
 
-  public async edit ({}: HttpContextContract) {
+  public async edit({ }: HttpContextContract) {
   }
 
-  public async update ({}: HttpContextContract) {
+  public async update({ }: HttpContextContract) {
   }
 
-  public async destroy ({}: HttpContextContract) {
+  public async destroy({ }: HttpContextContract) {
   }
 }
