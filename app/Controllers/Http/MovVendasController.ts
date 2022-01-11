c04354a43fef1cf54a107fad0588115f83dcc047
EriksJunior <EriksJunior13@gmail.com>
2022-01-10 23:18:31 -0300

    commit

--- app/Controllers/Http/MovVendasController.ts
+++ app/Controllers/Http/MovVendasController.ts
@@ -1,8 +1,33 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import MovVenda from '../../Models/MovVenda'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class MovVendasController {
   public async index({ }: HttpContextContract) {
+    try {
+      const dataProduct = await MovVenda.all()
+      console.log(dataProduct)
+      return dataProduct
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async getProductById({ params }: HttpContextContract) {
+    try {
+      const dataProductById = await Database.rawQuery(`select * from mov_vendas where id_venda = ${params.id}`)
+      console.log(dataProductById)
+      return dataProductById
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async getSalesById({ params }: HttpContextContract) {
+    const dataSalesById = await MovVenda.findOrFail(params.id)
+    console.log(dataSalesById)
+    return dataSalesById
   }
 
   public async create({ }: HttpContextContract) {
--- start/routes/mov_venda.ts
+++ start/routes/mov_venda.ts
@@ -1,3 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/movVenda', 'MovVendasController.store')
+Route.get('/movVenda/venda/:id', 'MovVendasController.getProductById')
+Route.get('/movVenda/:id', 'MovVendasController.getSalesById')
